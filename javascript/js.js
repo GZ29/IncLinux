541cc78dc0780cf827bb8cc72453f91f1cdec636
GZ29 <gabriel2005.sks@gmail.com>
2022-01-15 18:23:10 +0100

    Editttttttttttttttttttttttttttttt

---
+++ javascript/js.js
@@ -0,0 +1,11 @@
+AnimateTransition({
+    container: container,
+    blockIn: popup,
+    animation: 'bounce-in',
+    onTransitionStart: function (blockIn, blockOut, container, event) {
+
+    },
+    onTransitionEnd: function (blockIn, blockOut, container, event) {
+
+    }
+});
